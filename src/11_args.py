e699fe56470d2865022e24b405377f0bb0f009c2
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-01 14:43:37 +0100

    Add f2 function

--- src/11_args.py
+++ src/11_args.py
@@ -16,6 +16,9 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+def f2(*args):
+  return sum(args)
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
